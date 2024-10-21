e5b32a551845e10f74cd4f7e763509ef599864ae
pyhubexcel <pyhubexcel@gmail.com>
2024-10-21 10:28:59 +0200

    updated code

--- migrations/env.py
+++ migrations/env.py
@@ -2,9 +2,12 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+import os
 from alembic import context
 from app.models import FlightBookingInfo, User, FlightInfo, VerificationToken
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,6 +17,11 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+config.set_main_option(
+    'sqlalchemy.url',
+    os.getenv('SQLALCHEMY_DATABASE_URL', 'default_value_if_not_set')
+)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
